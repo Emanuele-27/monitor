52aabf33b06adedf95b328244671e5b8da519c61
Emanuele <emanuele.spreca@emea.nttdata.com>
2022-07-11 16:22:24 +0200

    not bad block ui bibloblu

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Chart from 'chart.js/auto';
 import './home.css';
-import {monitorClient, monitorStatClient, monitorAccountabilityClient} from 'clients/clients';
+import { monitorClient, monitorStatClient, monitorAccountabilityClient } from 'clients/clients';
 
 class Home extends Component {
 
@@ -74,16 +74,32 @@ class Home extends Component {
                 }
             }
         };
+
+        this.createdCharts = false;
+
+        this.createCharts = this.createCharts.bind(this);
+        this.createChart = this.createChart.bind(this);
     }
 
-    async componentDidMount() {
-        let monitorStatus = await monitorClient.welcomeTest();
-        let monitorStatStatus = await monitorStatClient.welcomeTest();
-        let monitorAccountabilityStatus = await monitorAccountabilityClient.welcomeTest();
+    async createCharts() {
 
-        this.createChart('monitor-pie', monitorStatus && monitorStatus.isOnline === true ? this.dataOK : this.dataKO);
-        this.createChart('monitor-stat-pie', monitorStatStatus && monitorStatStatus.isOnline === true ? this.dataOK : this.dataKO);
-        this.createChart('monitor-acc-pie', monitorAccountabilityStatus && monitorAccountabilityStatus.isOnline === true ? this.dataOK : this.dataKO);
+        // Crea i chart solo se il click espande l'accordion e se non sono già stati creati per quest'istanza
+        if (document.getElementsByClassName('collapsed').length === 0 && !this.createdCharts) {
+
+            this.createCharts = true;
+
+            this.props.blockContent();
+
+            let monitorStatus = await monitorClient.welcomeTest();
+            let monitorStatStatus = await monitorStatClient.welcomeTest();
+            let monitorAccountabilityStatus = await monitorAccountabilityClient.welcomeTest();
+
+            this.createChart('monitor-pie', monitorStatus && monitorStatus.isOnline === true ? this.dataOK : this.dataKO);
+            this.createChart('monitor-stat-pie', monitorStatStatus && monitorStatStatus.isOnline === true ? this.dataOK : this.dataKO);
+            this.createChart('monitor-acc-pie', monitorAccountabilityStatus && monitorAccountabilityStatus.isOnline === true ? this.dataOK : this.dataKO);
+
+            this.props.unblockContent();
+        }
     }
 
     createChart(id, data) {
@@ -92,14 +108,14 @@ class Home extends Component {
             chartStatus.destroy();
         new Chart(id, data);
     }
-    
+
     render() {
         return (
             <div>
                 <div className="accordion" id="home-accordion">
                     <div className="accordion-item">
                         <h3 className="accordion-header" id="home-accordion-heading">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#div-collapsible-1" aria-controls="div-collapsible-1">Monitor</button>
+                            <button className="accordion-button collapsed" onClick={this.createCharts} type="button" data-bs-toggle="collapse" data-bs-target="#div-collapsible-1" aria-controls="div-collapsible-1">Monitor</button>
                         </h3>
                         <div id="div-collapsible-1" className="accordion-collapse collapse" aria-labelledby="home-accordion-heading" data-bs-parent="#home-accordion">
                             <div id="pies" className="accordion-body">
--- src/layout/content.js
+++ src/layout/content.js
@@ -9,47 +9,76 @@ import Elenco from "components/elenco/elenco";
 import Giornale from "components/giornale/giornale";
 import Home from "components/home/home";
 import Rpt from "components/rpt/rpt";
+
+import { BlockUI } from 'primereact/blockui';
+
 import './content.css';
 
+export const blockContent = () => this.setState({ blockedContent: true });
+
 class Content extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            blockedContent: false
+        };
+        this.blockContent = this.blockContent.bind(this);
+        this.unblockContent = this.unblockContent.bind(this);
+    }
+
+
+    blockContent() {
+        this.setState({
+            blockedContent: true
+        });
+    }
+
+    unblockContent() {
+        this.setState({
+            blockedContent: false
+        });
+    }
+
     render() {
         return (
-            <div>
-                <div className="container-fluid" style={{ width:"85%", marginTop: "2rem", marginBottom: "2rem" }}>
-                    <div className="row">
-                        <div className="col-12 col-md-4 col-xl-3">
-                            <Link to="/home" id="linkHome" className="btn btn-outline-primary btn-lg entrypoint">
-                                HOME
-                            </Link>
-                        </div>
-                        <div className="col-12 col-md-4 col-xl-3">
-                            <Link to="/rpt" id="linkRpt" className="btn btn-outline-primary btn-lg entrypoint">
-                                RPT SENZA RT
-                            </Link>
-                        </div>
-                        <div className="col-12 col-md-4 col-xl-3">
-                            <Link to="/elenco" id="linkElenco" className="btn btn-outline-primary btn-lg entrypoint">
-                                ELENCO FLUSSI
-                            </Link>
-                        </div>
-                        <div className="col-12 col-md-4 col-xl-3">
-                            <Link to="/giornale" id="linkGiornale" className="btn btn-outline-primary btn-lg entrypoint">
-                                GIORNALE EVENTI
-                            </Link>
+            <BlockUI blocked={this.state.blockedContent} >
+                <div>
+                    <div className="container-fluid" style={{ width: "85%", paddingTop: "2rem"}}>
+                        <div className="row">
+                            <div className="col-12 col-md-4 col-xl-3">
+                                <Link to="/home" id="linkHome" className="btn btn-outline-primary btn-lg entrypoint">
+                                    HOME
+                                </Link>
+                            </div>
+                            <div className="col-12 col-md-4 col-xl-3">
+                                <Link to="/rpt" id="linkRpt" className="btn btn-outline-primary btn-lg entrypoint">
+                                    RPT SENZA RT
+                                </Link>
+                            </div>
+                            <div className="col-12 col-md-4 col-xl-3">
+                                <Link to="/elenco" id="linkElenco" className="btn btn-outline-primary btn-lg entrypoint">
+                                    ELENCO FLUSSI
+                                </Link>
+                            </div>
+                            <div className="col-12 col-md-4 col-xl-3">
+                                <Link to="/giornale" id="linkGiornale" className="btn btn-outline-primary btn-lg entrypoint">
+                                    GIORNALE EVENTI
+                                </Link>
+                            </div>
                         </div>
                     </div>
+                    <div className="container" style={{ marginTop: "2rem", paddingBottom: "2rem"}}>
+                        <Routes>
+                            <Route exact path="/" element={<Home blockContent={this.blockContent} unblockContent={this.unblockContent} />} />
+                            <Route path="/home" element={<Home blockContent={this.blockContent} unblockContent={this.unblockContent} />} />
+                            <Route path="/rpt" element={<Rpt blockContent={this.blockContent} unblockContent={this.unblockContent} />} />
+                            <Route path="/elenco" element={<Elenco blockContent={this.blockContent} unblockContent={this.unblockContent} />} />
+                            <Route path="/giornale" element={<Giornale blockContent={this.blockContent} unblockContent={this.unblockContent} />} />
+                        </Routes>
+                    </div>
                 </div>
-
-                <div className="container" style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/rpt" element={<Rpt />} />
-                        <Route path="/elenco" element={<Elenco />} />
-                        <Route path="/giornale" element={<Giornale />} />
-                    </Routes>
-                </div>
-            </div>
+            </BlockUI>
         );
     }
 
